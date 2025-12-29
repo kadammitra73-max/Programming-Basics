2cbaa79ce59505ea440bafdd23c1e87a631b2ce9
Kadam Mitra <kadammitra73@gmail.com>
2025-12-29 19:27:13 +0530

    Update datatype.c

---
+++ datatype.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int age=10;
+	float price=54.5;
+	char grade='a';
+	printf("age:%d\n",age);
+	
+	printf("price:%f\n",price);
+	
+	printf("grade:%c\n",grade);
+	
+	return 0;
+}
