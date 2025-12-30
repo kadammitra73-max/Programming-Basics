cff46cd5a456ed4b111a5a5c78f551570ad67459
Kadam Mitra <kadammitra73@gmail.com>
2025-12-30 17:42:41 +0530

    Create arithmetic.c

---
+++ arithmetic.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int a=50,b=20,;
+	printf("sum=%d\n",a+b);
+	printf("sub=%d\n",a-b);
+	printf("mul=%d\n",a*b);
+	printf("div=%d\n",a/b);
+	printf("mod=%d\n",a%b);
+	return 0;
+}
+	
