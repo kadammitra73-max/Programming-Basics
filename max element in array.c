ef08ca3666807e2d4e58b7288cee3a98a334ec6e
Kadam Mitra <kadammitra73@gmail.com>
2026-01-03 19:24:57 +0530

    Create max element in array.c

---
+++ max element in array.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+	int a[5],i,max;
+	printf("enter the elements:");
+	for(i=0;i<5;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	max=a[0];
+	for(i=0;i<5;i++)
+	{
+		if(a[i]>max)
+		{
+			max=a[i];
+		}
+	}
+	printf("max=%d\n",max);
+	return 0;
+}
