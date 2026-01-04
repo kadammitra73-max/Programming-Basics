08978c527e95f5bf91a6103faa9a8b4a9b287a58
Kadam Mitra <kadammitra73@gmail.com>
2026-01-04 16:41:01 +0530

    Add copied_strings.c for string copying example

---
+++ strings/copied_strings.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char str1[20],str2[20];
+	printf("enter the strings:");
+	scanf("%s",str1);
+	strcpy(str2,str1);
+	printf("copied string=%s",str2);
+	return 0;
+}
