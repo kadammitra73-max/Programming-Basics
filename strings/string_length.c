36709291056782ada888abf67c092637ce2345ed
Kadam Mitra <kadammitra73@gmail.com>
2026-01-04 15:46:33 +0530

    Add string length calculation program

---
+++ strings/string_length.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[100];
+
+    printf("Enter a string: ");
+    gets(str);
+
+    printf("Length of string = %lu", strlen(str));
+
+    return 0;
+}
